483316f3ecd76c5edc776e9877428aa3f4e9356e
winvm <ridtt.tz@gmail.com>
2025-06-01 14:04:15 +0300

    ignoring yarn cache

--- src-tauri/src/secure_link_windows_service.rs
+++ src-tauri/src/secure_link_windows_service.rs
@@ -7,15 +7,19 @@ use crate::secure_link_client::{SecureLinkClient, SecureLinkClientError};
 
 pub struct SecureLinkWindowsService {
     secure_link_server_host: String,
-    secure_link_server_port: u16
+    secure_link_server_port: u16,
+    auth_token: String,
+    service_log_file_path: String,
 }
 
 impl SecureLinkWindowsService {
     
-    pub fn new(secure_link_server_host: &str, secure_link_server_port: u16) -> Self {
+    pub fn new(secure_link_server_host: &str, secure_link_server_port: u16, auth_token: &str, service_log_file_path: &str) -> Self {
         SecureLinkWindowsService {
             secure_link_server_host: secure_link_server_host.to_string(),
             secure_link_server_port,
+            auth_token: auth_token.to_string(),
+            service_log_file_path: service_log_file_path.to_string(),
         }
     }
     
@@ -24,18 +28,13 @@ impl SecureLinkWindowsService {
 #[async_trait]
 impl SecureLinkClient for SecureLinkWindowsService {
     async fn start(&self) -> Result<(), SecureLinkClientError> {
-
-        let token = 
-            CredentialManager::load(SECURE_LINK_APP_AUTH_TOKEN_KEY).expect("Failed to load token");
-        
-        let service_log_file_path = r"E:\source\secure_link_app\service_log.txt";
         
         let start_service_result =
             secure_link_windows_service_manager::start_service(
                 self.secure_link_server_host.as_str(),
                 self.secure_link_server_port,
-                &token,
-                &service_log_file_path
+                &self.auth_token,
+                &self.service_log_file_path,
             );
         
         match start_service_result { 
